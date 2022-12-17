955b8e7375275f1d0f3dd1332410598ec650c1df
sakou-dtws <sakou@dtws-work.in>
2022-12-17 15:33:23 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://ringoku.github.io",
+  base: "/my-repo",
+});
