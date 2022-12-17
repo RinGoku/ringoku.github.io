0782b526fe7075cdb68317d72cea3a027ed8693c
RinGoku <yuyu80105@gmail.com>
2022-12-17 15:37:37 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import { defineConfig } from "astro/config";
-
 export default defineConfig({
   site: "https://ringoku.github.io",
   base: "/my-repo",
